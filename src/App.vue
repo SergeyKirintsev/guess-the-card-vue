9e1d11302c9f72fa4d6873610e369f09112f693b
SergeyKirintsev <kserg80@gmail.com>
2021-03-03 09:55:50 +0300

    Добавить счётчик ходов

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,9 @@
         <h2>Ход игры</h2>
         <p>Таймер:</p>
         <p>Лучший результат:</p>
-        <p>Кол-во ходов:</p>
+        <p>Кол-во ходов: <mark>{{ stepCount }}</mark></p>
+        <p>Угадано: {{guessCardsCount}}</p>
+        <p>Осталось: {{cardsCount - guessCardsCount}}</p>
       </div>
 
     </section>
@@ -75,21 +77,30 @@ export default {
     return {
       cards: [],
       cardsCount: "10",
-      compareCardsArr: []
+      compareCardsArr: [],
+      stepCount: 0,
+      guessCardsCount: 0
     };
   },
 
   methods: {
-    startGame() {
+    resetGame() {
       this.cards = [];
+      this.stepCount = 0;
+      this.guessCardsCount = 0;
+    },
+
+    startGame() {
+      this.resetGame();
       setTimeout( ()=> {
         this.cards = [...makeCards(this.cardsCount)];
       }, 0)
     },
 
     compareCards(id, color, targetEl) {
-      console.log(id, color);
+      // console.log(id, color);
       if (this.compareCardsArr.length === 0) {
+        this.stepCount++;
         this.compareCardsArr = [id, color, targetEl];
         return;
       }
@@ -97,11 +108,16 @@ export default {
       if (id === compareId) {
         return;
       } else {
+        this.stepCount++;
         if (color === compareColor) {
-          console.log("Цвет совпал");
+          // console.log("Цвет совпал");
           targetEl.classList.add("cards__item_disabled");
           compareTargetEl.classList.add("cards__item_disabled");
           this.compareCardsArr = [];
+          this.guessCardsCount += 2;
+          setTimeout(()=>{
+            this.checkStopGame();
+          }, 0)
         } else {
           targetEl.style.backgroundColor = "black";
           compareTargetEl.style.backgroundColor = "black";
@@ -120,88 +136,16 @@ export default {
       targetEl.style.backgroundColor = color;
       setTimeout(() => {
         this.compareCards(id, color, targetEl);
-      }, 1500);
+      }, 1100);
+    },
+
+    checkStopGame() {
+      if (this.cardsCount == this.guessCardsCount) {
+        alert(`Игра окончена. Ваш результат ${this.stepCount} ходов`);
+      }
     }
   }
 };
 </script>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-}
-
-.page {
-  max-width: 560px;
-  margin: 0 auto;
-  font-family: 'Helvetica Neue', Arial, sans-serif;
-}
-
-h1 {
-  margin: 15px auto;
-  text-align: center;
-}
-
-.game {
-  display: flex;
-  justify-content: space-between;
-}
-
-.game__form {
-  margin-left: 20px;
-}
-
-.game__config {
-  max-width: 250px;
-}
-
-.game__result {
-  max-width: 250px;
-}
-
-input {
-  /*margin: 20px;*/
-  width: 200px;
-}
-
-.game__start-btn {
-  border: 3px solid #006089;
-  border-radius: 5px;
-  font-size: 15px;
-  padding: 15px 50px;
-  margin: 20px;
-}
-
-.cards__item {
-  width: 100px;
-  height: 100px;
-  color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 24px;
-  background-color: black;
-  /*border-radius: 5px;*/
-  transition: transform 0.4s, background-color 0.7s;
-}
-
-.cards__item:hover {
-  transform: scale(1.05);
-}
-
-.cards__list {
-  list-style-type: none;
-  display: grid;
-  justify-content: center;
-  grid-template-columns: repeat(auto-fit, 100px);
-  gap: 15px;
-  margin: 0;
-  padding: 0;
-}
-
-.cards__item_disabled {
-  pointer-events: none;
-  opacity: 0.2;
-}
-</style>
+<style src="./app.css"></style>
